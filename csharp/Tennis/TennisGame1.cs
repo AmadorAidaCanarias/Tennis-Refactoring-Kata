ede9013dd712a65d39e8d7c334ec176a13cf62f7
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-23 08:06:29 +0100

    cambiar último switch por una comprobación en una línea

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -1,39 +1,35 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Tennis
-{
-    public class Player
-    {
+namespace Tennis {
+    public class Player {
         public string Name { get; }
         public int Score { private set; get; }
 
-        public Player(string name, int score)
-        {
+        public Player(string name, int score) {
             Name = name;
             Score = score;
         }
 
-        public void IncScore(int value) {
-            Score += value;
+        public void IncScore(int value, string name) {
+            if (Name == name) {
+                Score += value;
+            }
         }
     }
 
-    public class ScoreBoard
-    {
-        private readonly string[] _scoreNames = new[] {"Love", "Fifteen", "Thirty", "Forty"};
+    public class ScoreBoard {
+        private readonly string[] _scoreNames = new[] { "Love", "Fifteen", "Thirty", "Forty" };
         private readonly Dictionary<int, string> _scoreAdvantage = new() { { -1, "Advantage player2" }, { 1, "Advantage player1" } };
 
-        public string CurrentScore(int player1Score, int player2Score)
-        {
+        public string CurrentScore(int player1Score, int player2Score) {
             int difference = (player1Score - player2Score);
 
-            if (difference == 0)
-            {
+            if (difference == 0) {
                 return (player1Score < 3) ? _scoreNames[player1Score] + "-All" : "Deuce";
             }
 
-            if (player1Score <= 3 & player2Score <= 3) { 
+            if (player1Score <= 3 & player2Score <= 3) {
                 return _scoreNames[player1Score] + "-" + _scoreNames[player2Score];
             }
 
@@ -41,23 +37,16 @@ namespace Tennis
                 return _scoreAdvantage[difference];
             }
 
-            switch (difference) {
-                case >= 2:
-                    return "Win for player1";
-                default:
-                    return "Win for player2";
-            }
+            return difference >= 2 ? "Win for player1" : "Win for player2";
         }
     }
 
-    public class TennisGame1 : ITennisGame
-    {
+    public class TennisGame1 : ITennisGame {
         private readonly Player player1;
         private readonly Player player2;
         private readonly ScoreBoard scoreBoard;
 
-        public TennisGame1(string player1Name, string player2Name)
-        {
+        public TennisGame1(string player1Name, string player2Name) {
             player1 = new Player(player1Name, 0);
             player2 = new Player(player2Name, 0);
             scoreBoard = new ScoreBoard();
@@ -65,14 +54,11 @@ namespace Tennis
 
         public void WonPoint(string playerName)
         {
-            if (playerName == player1.Name)
-                player1.IncScore(1);
-            else
-                player2.IncScore(1);
+            player1.IncScore(1, playerName);
+            player2.IncScore(1, playerName);
         }
 
-        public string GetScore()
-        {
+        public string GetScore() {
             return scoreBoard.CurrentScore(player1.Score, player2.Score);
         }
     }
